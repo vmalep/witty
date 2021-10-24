d975777706077262892529b2562b9c1cf98c3928
JanaKudlickova <jana-kudlickova_student2021@wilder.school>
2021-10-24 19:29:45 +0200

    "added a state userName"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import {useState} from "react";
 import Header from "./components/Header.js"
+import InputBox from "./components/InputBox.js"
+
 
 function App() {
+  const [userName, setUserName] = useState(null);
+  
   return (
-    <div>
-      <Header />
-    </div>
+      <div>
+        <Header />
+        {userName ? (
+          <></>
+        ) : <InputBox setUserName={setUserName} />}
+      </div> 
   );
 }
 
