0e9e4343ed9cd54aaca161687fc3ff6f6a8b51d6
Pierre van Male <pierre-van-male_student2021@wilder.school>
2021-10-26 18:00:59 +0200

    options added in boxes

--- src/components/QuizzCard.js
+++ src/components/QuizzCard.js
@@ -1,15 +1,34 @@
+import { useEffect, useState } from "react";
+
 const QuizzCard = (props) => {
-    console.log("props = ", props)
+
+    console.log("props: ", props)
+
+    const addCorrectAnswerAndShuffle = (correct_answer) => {
+        const newPossibleAnswers = possibleAnswers.slice();
+        newPossibleAnswers.push(correct_answer);
+        newPossibleAnswers.sort(() => Math.random() - 0.5);
+        setPossibleAnswers(newPossibleAnswers);
+    }
+
+    const [possibleAnswers, setPossibleAnswers] = useState(props.quizzQuestion.incorrect_answers)
+
+    useEffect(() => {
+        addCorrectAnswerAndShuffle(props.quizzQuestion.correct_answer)
+    }, [])
 
     return (
         <>
-            <p>{props.quizzQuestion.question}</p>
-            <p>{props.quizzQuestion.correct_answer}</p>
-            <ul>{props.quizzQuestion.incorrect_answers.map((element, index) => {
+            <p>score: {props.score}</p>
+            <p>question {props.questCount + 1}: {props.quizzQuestion.question}</p>
+            <div>
+                {possibleAnswers.map((element, index) => {
                 return (
-                    <li>{element}</li>
+                    <button key={index} onClick={() => props.checkAnswer(element)}>{element}</button>
                 )
-            })}</ul>
+                })}
+            </div>
+
         </>
     )
 }
