bac7ad34ed2a5ea2f1dd8adbd747b3a49d69e452
gauthier-chatel_student2021@wilder.school <88785506+gc2211@users.noreply.github.com>
2021-10-26 22:26:37 +0200

    Creation Category.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Avatar from './components/Avatar';
 function App() {
   return (
     <div className="App">
-       <Avatar />
+       <Avatar/>
     </div>
   );
 }
---
+++ src/components/Category.js
@@ -0,0 +1,31 @@
+import './App.css'
+
+const Category = () => {
+    return (
+        <>
+          <div class="usernamebox">
+              <p></p>
+              </div>
+              <div>
+              <p>Select your Categories</p>
+              </div>
+              <div class="categorybox1">
+              <p></p>
+              </div>
+              <div class="categorybox2">
+              <p></p>
+              </div>
+              <div class="categorybox3">
+              <p></p>
+              </div>
+              <div class="categorybox4">
+              <p></p>
+              </div>
+        </>
+    )
+}
+
+export default Category;
+
+
+
