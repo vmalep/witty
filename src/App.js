65e1564937b67d55d918ce89d8cb496b5dbcd68c
Pierre van Male <pierre-van-male_student2021@wilder.school>
2021-10-26 22:15:09 +0200

    adding Witty as document.title

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Avatar from "./components/Avatar"
 import InputBox from "./components/InputBox"
 import QuizzSection from './components/QuizzSection';
 
+document.title = "Witty"
 
 function App() {
   const [userName, setUserName] = useState(null);
