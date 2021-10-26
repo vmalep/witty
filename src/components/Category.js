f502ffdc53cb2f77b6f2739dddb8139f6825f7f4
gauthier-chatel_student2021@wilder.school <88785506+gc2211@users.noreply.github.com>
2021-10-26 23:34:29 +0200

    Added levels buttons and h2 Select your Category

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,13 +1,26 @@
 const Category = () => {
-const categories = ["history", "sport","art"]
+const categories = ["History", "Sport","Art","Music"]
+const level = ["Easy","Medium","Hard"]
+            
     return (
+        <>
+        <h2>Select your Category</h2>
+        <div class = "level">
+            {level.map((element,index) => {
+                return(
+                    <button key={index}>{element}</button>
+                )
+                })}
+        </div>
+        
           <div class="usernamebox">
               {categories.map((element , index) => {
                   return(
-                      <button key={index} >{element}</button>
+                      <button key={index}>{element}</button>
                   )
               })}
               </div>
+       </>
     )
 }
 
