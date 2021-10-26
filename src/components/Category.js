c6f21f44222acb2afca8ef227b6ea574c3a2e96d
gauthier-chatel_student2021@wilder.school <88785506+gc2211@users.noreply.github.com>
2021-10-26 23:59:22 +0200

    Added level buttons and h2 for Category

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,17 +1,27 @@
-
 const Category = () => {
-    const categories = ["history", "sport", "art"]
-    return (
-        <div class="usernamebox">
-            {categories.map((element, index) => {
-                return (
-                    <button key={index} >{element}</button>
-                )
-            })}
-        </div>
-    )
-}
-
-export default Category;
-
+    const categories = ["History", "Sport","Art","Music"]
+    const level = ["Easy","Medium","Hard"]
+    
+        return (
+            <>
+            <h2>Select your Category</h2>
+            <div class = "level">
+                {level.map((element,index) => {
+                    return(
+                        <button key={index}>{element}</button>
+                    )
+                    })}
+            </div>
+    
+              <div class="usernamebox">
+                  {categories.map((element , index) => {
+                      return(
+                          <button key={index}>{element}</button>
+                      )
+                  })}
+                  </div>
+           </>
+        )
+    }
+export default Category ;
 /*onClick={() => props.checkAnswer(element)}*/
