254775789e9152bb7216f44d74c0b3551b6b337c
gauthier-chatel_student2021@wilder.school <88785506+gc2211@users.noreply.github.com>
2021-10-27 10:56:11 +0200

    Added button next

--- src/components/Category.js
+++ src/components/Category.js
@@ -25,6 +25,8 @@
                       )
                   })}
                   </div>
+                  <button className="button"
+                onClick={handleClick}>NEXT</button>
            </>
         )
     }
