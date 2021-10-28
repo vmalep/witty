3ff04e713d8b5f8f471a534e5a1bc575ff46abd0
Pierre van Male <pierre-van-male_student2021@wilder.school>
2021-10-28 08:46:35 +0200

    creation of a TriviaApi file to give the names of categories and correspoding nb

---
+++ src/utils/TriviaApi.js
@@ -0,0 +1,19 @@
+export const TriviaAPI = {
+    baseUrl: "https://opentdb.com/api.php",
+    categories: [
+        {catName: "History", catNb: "23"},
+        {catName: "General Knowledge", catNb: "9"},
+        {catName: "Books", catNb: "10"},
+        {catName: "Film", catNb: "11"},
+        {catName: "Music", catNb: "12"},
+        {catName: "SCience & Nature", catNb: "17"},
+        {catName: "Computers", catNb: "18"},
+        {catName: "Mathematics", catNb: "19"},
+        {catName: "Mythology", catNb: "20"},
+        {catName: "Sports", catNb: "21"},
+        {catName: "Geography", catNb: "22"},
+        {catNane: "History", catNb: "23"},
+        {catName: "Politics", catNb: "24"},
+        {catName: "Art", catNb: "25"}
+        ]
+}
