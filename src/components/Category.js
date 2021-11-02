73012bb22b3c728367956b1fe1e8df8e6d6936e9
JanaKudlickova <jana-kudlickova_student2021@wilder.school>
2021-11-02 20:46:05 +0100

    imported Difficulty + edited button/handleClick

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,19 +1,22 @@
  import {useState} from "react";
  import { TriviaAPI } from "../utils/TriviaAPI";
+ import Difficulty from "./Difficulty";
  
  const Category = (props) => {
-    const {userName, selectedAvatar, setAppStep, selectedCategory, setSelectedCategory} = props
-    const level = ["Easy","Medium","Hard"]
-
-
+    const {userName, selectedAvatar, setAppStep, selectedCategory, setSelectedCategory, difficultyLevel} = props
+    const [nextBtnDisabled, setNextBtnDisabled] = useState([true, 'grey']);
 
     const handleClick = () => {
+        if(!difficultyLevel || !selectedCategory) {
+            setNextBtnDisabled(true, "grey")
+        }
+        setNextBtnDisabled(false)
         setAppStep(2)
     }
 
     const handleCategory = (element) => {
         setSelectedCategory(element.catNb);
-        setAppStep(2)
+        setNextBtnDisabled(false)
     }
     
 
@@ -23,15 +26,7 @@
                     <p className="current-player-name">{userName}</p>
                     <img className="current-player-avatar" src={selectedAvatar.src} alt={selectedAvatar.alt} />
                 </div>
-                
-                <h2>Select your Difficulty</h2>
-                <div className = "level">
-                    {level.map((element,index) => {
-                        return(
-                            <button key={index}>{element}</button>
-                        )
-                    })}
-                </div>
+                <Difficulty />
                 <h2>Select your Category</h2>
                 <div className="usernamebox">
                     {TriviaAPI.map((element , index) => {
@@ -39,14 +34,17 @@
                             <button
                                     key={index}
                                     onClick={() => handleCategory(element)} 
-                                    style={{backgroundColor: element.color}}
+                                    style={{backgroundColor: element.color}}      
                             >{element.catName}</button>   
                         )
                     })}
                           
                 </div>
                 <button className="button"
-                    onClick={handleClick}>NEXT</button>
+                    onClick={handleClick}
+                    disabled={nextBtnDisabled[0]}
+                    style={{backgroundColor: nextBtnDisabled[1]}}
+                    >NEXT</button>
            </>
         )
     }
