e76876d13840fe808812deb9cf47bdf383f2aae7
Pierre van Male <pierre-van-male_student2021@wilder.school>
2021-11-03 09:49:02 +0100

    ok for me

--- src/components/UserInit.js
+++ src/components/UserInit.js
@@ -1,34 +1,37 @@
 import {useEffect, useState} from "react"
 import App from "../App"
 import Avatar from "./Avatar"
-import UserName from "./UserName"
+import NickName from "./NickName"
 
 const UserInit = (props) => {
-
+    
     const {
         selectedAvatar, 
         setSelectedAvatar,
         userName,
         setUserName, 
         setAppStep} = props
-
-    const [nextBtnDisabled, setNextBtnDisabled] = useState([true, 'grey'])
-    
+        
+        const [nextBtnDisabled, setNextBtnDisabled] = useState([true, 'grey'])
+        
     // Each time selectedAvatar or userName is modified, check if both are filled and if so, activate the Next button
     useEffect(() =>{
-        (selectedAvatar && userName) && setNextBtnDisabled(false, "blue")
-    }, [selectedAvatar, userName])
+        if((Object.keys(selectedAvatar).length !== 0) && userName) {
+            console.log("enabling the next button")
+            setNextBtnDisabled([false])
+        } else {
+            console.log("disabling the next button")
+            setNextBtnDisabled([true, 'grey'])
+        }
 
-    const handleNext = () => {
-        setAppStep(1)
-    }
+    }, [selectedAvatar, userName])
 
     return (
         <div className="flex-column">
             <Avatar selectedAvatar={selectedAvatar} setSelectedAvatar={setSelectedAvatar}/> 
-            <UserName setUserName={setUserName} etAppStep={setAppStep} />
+            <NickName setUserName={setUserName} />
             <button
-                onClick={handleNext}
+                onClick={() => setAppStep(1)}
                 disabled={nextBtnDisabled[0]}
                 style={{backgroundColor: nextBtnDisabled[1]}}
             >
