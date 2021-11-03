d64846eff6623af8b220ed919ee78835027a338f
Pierre van Male <pierre-van-male_student2021@wilder.school>
2021-11-03 19:14:20 +0100

    ongoing

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,5 +1,6 @@
  import {useState} from "react";
  import { TriviaAPI } from "../utils/TriviaAPI";
+ const colorConvert = require('color-convert');
  
  const Category = (props) => {
     const {selectedCategory, setSelectedCategory, setNextBtnDisabled} = props
@@ -14,11 +15,23 @@
                 <h2>Select your Category</h2>
                 <div className="usernamebox">
                     {TriviaAPI.map((element , index) => {
+
+                        console.log("element.color" + colorConvert.keyword.hsl(element.color))
+                        const fadedColor=[
+                            colorConvert.keyword.hsl(element.color)[0],
+                            colorConvert.keyword.hsl(element.color)[1],
+                            "50%"
+                        ]
+                        console.log("fadedColor" + fadedColor)
+
                         return(
                             <button
                                     key={index}
                                     onClick={() => handleCategory(element)} 
-                                    style={{backgroundColor: selectedCategory !== element.catNb ? element.color : 'purple'}} 
+                                    style={{backgroundColor: selectedCategory !== element.catNb 
+                                        ? element.color 
+                                        : `hsl(${fadedColor})`
+                                        }} 
                             >{element.catName}</button>   
                         )
                     })}         
