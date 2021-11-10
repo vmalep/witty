d2eff93df03280fb7b51862e764e2328c456a375
Pierre van Male <pierre-van-male_student2021@wilder.school>
2021-11-10 11:34:54 +0100

    first test

--- src/components/CountDownTimer.js
+++ src/components/CountDownTimer.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Countdown from 'react-countdown';
+import * as React from "react";
+import { render } from "react-dom";
 
 const CountDownTimer = () => {
-    // Random component
-    const Completionist = () => <span>Too late!</span>;
-    
-    ReactDOM.render(
-      (
-        <Countdown date={Date.now() + 5000}>
-          <Completionist />
-        </Countdown>
-      ),
-      document.getElementById('root')
-    );
+
+  const [counter, setCounter] = React.useState(10);
+  
+  React.useEffect(() => {
+    counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
+  }, [counter]);
+
+  return (
+    <div className="App">
+      <div>Countdown: {counter}</div>
+    </div>
+  );
 }
+
+/* const rootElement = document.getElementById("root");
+render(<App />, rootElement); */
 
 export default CountDownTimer
