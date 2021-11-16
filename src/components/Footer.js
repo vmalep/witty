98ae3928f8afc1c9b9272a82422ebcd7933ed526
Pierre van Male <pierre-van-male_student2021@wilder.school>
2021-11-16 16:33:29 +0100

    adding Trivia API link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,8 @@
 const Footer = () => {
     return (
     <footer>
-        <p className="footer-text">Created by Pierre, Jana and Gauthier. 2021</p> 
+        <p className="footer-text">Created by Pierre, Jana and Gauthier in 2021<br/>
+        <a href="https://opentdb.com/api_config.php">Trivia API</a></p>
     </footer>
     );
 };
